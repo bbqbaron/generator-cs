f57d00895d69deb48e118e14eeb1e34177487065
Eric Loren <osiris1723@gmail.com>
2015-02-14 21:24:05 -0500

    gulpfile fixes

--- app/templates/gulpfile.js
+++ app/templates/gulpfile.js
@@ -3,23 +3,29 @@ var gulp = require('gulp'),
     browserify = require('browserify'),
     transform = require('vinyl-transform');
 
+// TODO lessify
+
+// TODO this should be optional, since eg Django doesn't want you doing it this way
 gulp.task('html', function() {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-dev', ['ls', 'html'], function() {
-    var browserified = transform(function(filename) {
-        var b = browserify(filename);
-        return b.bundle();
-    });
+// TODO get rid of global transforms
+// TODO uglifyify
+var browserified = transform(function(filename) {
+    var b = browserify(filename);
+    return b.bundle();
+});
 
+// TODO parameterized output
+gulp.task('build-dev', ['html'], function() {
     return gulp.src('src/app/main.js')
         .pipe(browserified)
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['ls', 'html'], function() {
+gulp.task('build', ['html'], function() {
     return return gulp.src('src/app/main.js')
         .pipe(browserified)
         .pipe(uglify())
